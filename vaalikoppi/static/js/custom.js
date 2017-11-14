c7c67dec6ed51b3883708eb689fa90d51916a074
Kiikkilä Olli <olli.kiikkila@aalto.fi>
2017-11-14 22:35:45 +0200

    Token koodi näkyviin headeriin

--- vaalikoppi/static/js/custom.js
+++ vaalikoppi/static/js/custom.js
@@ -6,37 +6,37 @@
 SITE_ROOT_PATH = '/vaalikoppi/';
 
 function vote(votingId) {
-	
+
 	var form = $('#voting-form-' + votingId);
 	var selectedCandidateId = form.find('input[name=candidate]:checked').val();
-	
+
 	if (selectedCandidateId == null) {
 		return;
 	}
-	
+
 	var confirmation = confirm('Haluatko varmasti antaa äänesi?');
-	
+
 	if (!confirmation) {
 		return;
 	}
-	
+
 	form.find('input, button').prop('disabled', true);
-	
+
 	var query = $.post(SITE_ROOT_PATH + 'votings/' + votingId + '/vote/',
 		{ candidate : selectedCandidateId }
 	).done(function(data) {
 	}).fail(function(data) {
 		alert('Äänestäminen epäonnistui. Päivitä sivu ja yritä uudelleen!');
 	});
-	
+
 	refreshVotingList();
-	
+
 }
 
 function refreshVotingList(admin = false) {
-	
+
 	var votingArea = $('#voting-list-area');
-	
+
 	var query = $.get(SITE_ROOT_PATH + (admin ? 'admin/' : '' ) + 'votings/list/', function(data) {
 		votingArea.html(data);
 	})
@@ -46,9 +46,9 @@ function refreshVotingList(admin = false) {
 }
 
 function generateTokens(count) {
-	
+
 	$('#generate-tokens-button').prop('disabled', true);
-	
+
 	var query = $.post(SITE_ROOT_PATH + 'admin/tokens/generate/',
 		{ count : count }
 	).done(function(data) {
@@ -56,20 +56,20 @@ function generateTokens(count) {
 	}).fail(function(data) {
 		alert('Koodien generointi epäonnistui.');
 	});
-	
-	$('#generate-tokens-button').prop('disabled', false);	
+
+	$('#generate-tokens-button').prop('disabled', false);
 }
 
 function invalidateToken() {
-	
+
 	var token = $('#invalidate-token-field').val();
-	
+
 	if (token.length < 1) {
 		return;
 	}
-	
+
 	$('#invalidate-code-button').prop('disabled', true);
-	
+
 	var query = $.post(SITE_ROOT_PATH + 'invalidate/',
 		{ token : token }
 	).done(function(data) {
@@ -77,13 +77,13 @@ function invalidateToken() {
 	}).fail(function(data) {
 		alert('Koodin invalidointi epäonnistui. Tarkista koodi.');
 	});
-	
+
 	$('#invalidate-token-button').prop('disabled', false);
 }
 
 // Just for the UI. Everything is validated in the back-end...
 function checkVoterStatus(callback) {
-	
+
 	var query = $.getJSON(SITE_ROOT_PATH + 'user/status/')
 	.done(function(data) {
 		try {
@@ -92,6 +92,7 @@ function checkVoterStatus(callback) {
 				callback([ false, '' ]);
 			} else if (data.status === 1) {
 				if (data.activated === true && data.invalidated === false) {
+					$('#token_div').html(data.token);
 					callback([ true, data.token ]);
 				} else {
 					callback([ false, data.token ]);
@@ -99,7 +100,7 @@ function checkVoterStatus(callback) {
 			} else throw new Exception();
 		} catch (err) {
 			callback([ false, '' ]);
-		} 
+		}
 	})
 	.fail(function() {
 		alert('Tilan haku palvelimelta ei onnistunut. Koeta päivittää sivu.');
@@ -107,12 +108,12 @@ function checkVoterStatus(callback) {
 }
 
 function submitToken() {
-	
+
 	var token = $('#type-token-field').val();
 	var warning = $('#main-login-prompt .wrong-token-warning');
-	
+
 	warning.addClass('invisible');
-	
+
 	var query = $.post(SITE_ROOT_PATH + 'user/login/',
 		{ token : token }
 	).done(function(data) {
@@ -131,7 +132,7 @@ function toggleLoginPrompt() {
 }
 
 function closeVoting(votingId) {
-	
+
 	var query = $.post(SITE_ROOT_PATH + 'admin/votings/' + votingId + '/close/').done(function(data) {
 		refreshVotingList(true);
 	}).fail(function(data) {
@@ -140,7 +141,7 @@ function closeVoting(votingId) {
 }
 
 function openVoting(votingId) {
-	
+
 	var query = $.post(SITE_ROOT_PATH + 'admin/votings/' + votingId + '/open/').done(function(data) {
 		refreshVotingList(true);
 	}).fail(function(data) {
