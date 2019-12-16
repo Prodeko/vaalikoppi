1c8b802e4d72cf5afa2411eb28b561104096f887
rriski <timo.h.riski@outlook.com>
2019-12-16 16:43:11 +0200

    Update urls

--- election/urls.py
+++ election/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 
 app_name = "election"
 urlpatterns = [
-    path("vaalikoppi/", include("vaalikoppi.urls", namespace="vaalikoppi")),
+    path("", include("vaalikoppi.urls", namespace="vaalikoppi")),
     path("admin/", admin.site.urls),
 ]
