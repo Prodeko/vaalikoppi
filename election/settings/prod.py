8027ab5e759a805af0a3c72360d287c08cf06167
rriski <timo.h.riski@outlook.com>
2019-12-17 14:27:42 +0200

    Change ALLOWED_HOSTS and add whitenoise

--- election/settings/prod.py
+++ election/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vaalikoppi.azurewebsites.net', '127.0.0.1']
 
 DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
@@ -18,3 +18,10 @@ DATABASES = {
         "ATOMIC_REQUESTS": True,
     }
 }
+
+ 
+MIDDLEWARE += ("whitenoise.middleware.WhiteNoiseMiddleware",)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
