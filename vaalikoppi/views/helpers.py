aa4f2860bbf5fb73fbc291dead87ace39a926b96
Santeri Kivinen <skivinen@gmail.com>
2020-11-05 22:57:23 +0200

    Alias input validation and related UI hints

--- vaalikoppi/views/helpers.py
+++ vaalikoppi/views/helpers.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from vaalikoppi.models import Usertoken
+import re
 
 
 def is_valid_token(request, token_obj=None):
@@ -33,9 +34,11 @@ def get_active_tokens(request):
 
 # Raises exception
 def validate_register_alias(request, token_obj, alias):
+    alias_regex = r"^[A-Z0-9\u00C0-\u00D6\u00D8-u00DE][A-Z0-9\u00C0-\u00D6\u00D8-u00DE_\-]+$"
+    alias = alias.upper()
     alias_len = len(alias)
-     
-    if alias_len >=3 and alias_len <=20 and 0 == get_active_tokens(request).filter(alias=alias).count():
+
+    if alias_len >=3 and alias_len <=20 and bool(re.match(alias_regex, alias)) and 0 == get_active_tokens(request).filter(alias=alias).count():
         Usertoken.objects.filter(token=token_obj.token).update(alias=alias)
     else:
         raise Exception("Invalid alias provided")
