2bb5aa5791b84ae4e88614c36a4bb875050fe476
Timo Riski <timo.h.riski@aalto.fi>
2020-11-09 19:52:04 +0200

    Activate SessionLockMiddleware

--- election/settings/base.py
+++ election/settings/base.py
@@ -28,6 +28,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "election.middleware.SessionLockMiddleware",
 ]
 
 ROOT_URLCONF = "election.urls"
