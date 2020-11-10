bdc35bae18714425b97387f85bab0f06876435cd
Timo Riski <timo.h.riski@aalto.fi>
2020-11-10 19:21:52 +0200

    Add pytest-django

--- election/urls.py
+++ election/urls.py
@@ -1,4 +1,5 @@
 import debug_toolbar
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -9,6 +10,10 @@ urlpatterns = [
     path("", redirect_view),
     path("vaalikoppi/", include("vaalikoppi.urls", namespace="vaalikoppi")),
     path("admin/", admin.site.urls),
-    path("silk", include("silk.urls", namespace='silk')),
-    path("__debug__/", include(debug_toolbar.urls)),
 ]
+
+if settings.SHOW_DEBUG_TOOLBAR:
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
+
+if settings.SHOW_DJANGO_SILK:
+    urlpatterns.append(path("silk", include("silk.urls", namespace="silk")))
--- vaalikoppi/views/general.py
+++ vaalikoppi/views/general.py
@@ -1,12 +1,14 @@
 import random
 
 from django.shortcuts import render
-from silk.profiling.profiler import silk_profile
+# Set SHOW_DJANGO_SILK = True in election.settings.dev and
+# uncomment silk import and @silk_profile to enable django-silk
+# from silk.profiling.profiler import silk_profile
 from vaalikoppi.views.helpers import get_token_obj, is_valid_token
 from vaalikoppi.views.votings import votings_list_data
 
 
-@silk_profile(name='Index')
+# @silk_profile(name="Index")
 def index(request):
     data = {
         "is_valid_token": False,
