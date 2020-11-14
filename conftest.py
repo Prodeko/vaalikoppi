e840e71a7e660b2d8862c43927a92f52309557c4
Timo Riski <timo.h.riski@aalto.fi>
2020-11-14 15:21:30 +0200

    Fix typo and unused imports

--- conftest.py
+++ conftest.py
@@ -1,15 +1,13 @@
 import pytest
 from cacheops import invalidate_all
-from django.conf import settings
-from django.core.cache import cache
-from pytest_django.lazy_django import skip_if_no_django
 
 
 @pytest.fixture(autouse=True)
 def enable_db_access_for_all_tests(db):
     pass
 
+
 @pytest.fixture(autouse=True)
 def django_cache():
-    # Invalidate cache between test runss for consistency
+    # Invalidate cache between test runs for consistency
     invalidate_all()
