c139224f233eb07caf9930d6d4a249f633dac2ca
Joel Juhela <joel.juhela@protonmail.com>
2022-10-30 15:33:34 +0200

    Remove superuser creation from docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 timer="2"
 
 function wait_for_psql () {
-    until pg_isready --username=vaalikoppi --host=db 2>/dev/null; do
+    until pg_isready --username=postgres --host=db 2>/dev/null; do
     >&2 echo "Postgres is unavailable - sleeping for $timer seconds"
     sleep $timer
     done
@@ -22,12 +22,6 @@ python3 manage.py migrate
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
-
-# Create a superuser for development
-echo "Creating superuser..."
-python manage.py shell -c "from django.contrib.auth import get_user_model; \
-	User = get_user_model(); User.objects.filter(email='webbitiimi@prodeko.org').exists() or \
-	User.objects.create_superuser(username='webbitiimi', password='kananugetti', email='webbitiimi@prodeko.org')"
 
 nohup python manage.py sass vaalikoppi/static/scss vaalikoppi/static/css --watch &
 
