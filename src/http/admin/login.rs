9fece578a8f09e654984bf5fa6b4f29d7090443c
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2023-10-15 16:19:10 +0000

    Return expiry date from login handler, fix response type

--- src/http/admin/login.rs
+++ src/http/admin/login.rs
@@ -1,7 +1,6 @@
-use axum::extract::State;
+use axum::{extract::State, Json};
 use chrono::{Duration, Utc};
 use jsonwebtoken::{encode, EncodingKey, Header};
-use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 
 use crate::http::{
@@ -9,34 +8,51 @@ use crate::http::{
     AppState,
 };
 
-const TOKEN_EXPIRY_DURATION: i64 = 24;
+const TOKEN_EXPIRY_DURATION_HOURS: i64 = 24;
 
 #[derive(Deserialize)]
 struct LoginPayload {
     token: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize)]
 struct JsonWebTokenClaims {
     exp: i64,
     iat: i64,
 }
 
-fn json_web_token_login(state: State<AppState>, login_token: LoginPayload) -> Result<String> {
-    if login_token.token == state.config.admin_password {
-        let current_timestamp = Utc::now();
-        let claims = JsonWebTokenClaims {
-            exp: (current_timestamp + Duration::hours(TOKEN_EXPIRY_DURATION)).timestamp(),
-            iat: current_timestamp.timestamp(),
-        };
+#[derive(Serialize)]
+struct LoginResponse {
+    token: String,
+    exp: i64,
+}
 
-        return encode(
-            &Header::default(),
-            &claims,
-            &EncodingKey::from_secret(state.config.hmac_key.as_bytes()),
-        )
-        .map_err(|_| Error::LoginFail);
-    } else {
-        Err(Error::LoginFail)
+fn json_web_token_login(
+    state: State<AppState>,
+    Json(login_payload): Json<LoginPayload>,
+) -> Result<Json<LoginResponse>> {
+    if login_payload.token != state.config.admin_password {
+        return Err(Error::LoginFail);
     }
+
+    let current_timestamp = Utc::now();
+    let claims = JsonWebTokenClaims {
+        exp: (current_timestamp + Duration::hours(TOKEN_EXPIRY_DURATION_HOURS)).timestamp(),
+        iat: current_timestamp.timestamp(),
+    };
+
+    let token_result = encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(state.config.hmac_key.as_bytes()),
+    );
+
+    token_result
+        .map(|token| {
+            Json(LoginResponse {
+                token,
+                exp: claims.exp,
+            })
+        })
+        .map_err(|_| Error::LoginFail)
 }
