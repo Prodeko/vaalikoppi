a096129190236c417857adeee536bd7375dbed1b
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2024-03-24 15:05:32 +0000

    Define custom theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
 module.exports = {
   content: ['./src/templates/**/*.html'],
-  theme: {},
+  theme: {
+    colors: {
+      // Colors from Prodeko's style guide, https://static.prodeko.org/media/public/2023/12/14/brandidokumentti_final.pdf 
+      'on-primary': '#f6f6f6', // Prodeko's official "valkoinen 1"
+      'on-secondary': '#f6f6f6', // Prodeko's official "valkoinen 1"
+      'primary-container': '#ececec', // Prodeko's official "valkoinen 2"
+      'on-primary-container': '#000A14', // Prodeko's official "musta"
+      'primary': '#002e7d', // Prodeko's official "keskisininen"
+      'secondary': '#002851', // Prodekos official "tummansininen"
+    },
+    fontFamily: {
+      sans: ['Raleway', ...fontFamily.sans]
+    }  
+  },
 };
