0fb04aa8b2e59dbc7db0a97959818de0430128b5
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2024-10-06 13:36:06 +0000

    Clear database before running tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,7 +57,7 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: "cargo run",
+		command: "sqlx migrate revert && sqlx migrate run && cargo run",
 		url: `http://localhost:${port}`,
 	},
 });
