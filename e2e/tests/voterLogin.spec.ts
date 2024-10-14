1cc1aa76c13ed5ebaf410e3a85c3af0621af1d5d
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2024-10-14 14:38:15 +0000

    Capture generated tokens

--- e2e/tests/voterLogin.spec.ts
+++ e2e/tests/voterLogin.spec.ts
@@ -3,6 +3,5 @@ import { test } from "../fixtures";
 test("User can log in", async ({ adminLoginPage }) => {
 	const adminHomePage = await adminLoginPage.login();
 	const adminTokensPage = await adminHomePage.goToTokens();
-	const tokens = adminTokensPage.generateBulkTokens();
-	console.log(tokens);
+	const tokens = await adminTokensPage.generateBulkTokens();
 });
