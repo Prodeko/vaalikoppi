adc5b6767b194b877e32a7d24d910ac030c72aea
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2024-10-21 17:25:59 +0000

    Name test files descriptively

--- e2e/tests/createToken.spec.ts
+++ e2e/tests/createToken.spec.ts
@@ -1,10 +1,5 @@
 import { test } from "../fixtures";
 
-test("Can log in as admin", async ({ adminLoginPage }) => {
-	const adminVotingsPage = await adminLoginPage.login();
-	await adminVotingsPage.expectIsVisible();
-});
-
 test("Can create tokens", async ({ adminLoginPage }) => {
 	const votingsPage = await adminLoginPage.login();
 	await votingsPage.expectIsVisible();
