c7e8c2db13db220aba3cd0028813a313269cda42
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2024-10-26 11:39:27 +0000

    Close pages after test

--- e2e/fixtures.ts
+++ e2e/fixtures.ts
@@ -1,6 +1,7 @@
 import { test as base } from "@playwright/test";
 import { LoginPage } from "./page-objects/loginPage";
 import { AdminLoginPage } from "./page-objects/adminLoginPage";
+import { createPage } from "./utils";
 
 export interface LoginTest {
 	adminLoginPage: AdminLoginPage;
@@ -9,17 +10,17 @@ export interface LoginTest {
 
 export const test = base.extend<LoginTest>({
 	adminLoginPage: async ({ browser }, use) => {
-		const context = await browser.newContext();
-		const page = await context.newPage();
+		const page = await createPage(browser);
 		const adminLoginPage = new AdminLoginPage(page);
 		await adminLoginPage.goTo();
 		await use(adminLoginPage);
+		await page.close();
 	},
 	loginPage: async ({ browser }, use) => {
-		const context = await browser.newContext();
-		const page = await context.newPage();
+		const page = await createPage(browser);
 		const loginPage = new LoginPage(page);
 		await loginPage.goTo();
 		await use(loginPage);
+		await page.close();
 	},
 });
